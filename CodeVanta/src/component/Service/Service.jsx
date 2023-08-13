0b1954634d9f5ccb82db513efed67e38ef90f541
Horlaide22 <hartoke2sekinat@gmail.com>
2023-08-13 20:09:33 +0100

    technology responsiveness

--- CodeVanta/src/component/Service/Service.jsx
+++ CodeVanta/src/component/Service/Service.jsx
@@ -1,44 +1,110 @@
-import React from 'react'
+import React from "react";
 
 export const Service = () => {
   return (
-      <div>
-            <div className=''>
-        <div className='text-center flex flex-col gap-2'>
-           <p className='text-[4rem] text-[#225AA5] font-normal '>SERVICES</p> 
-           <p className='text-[1.5rem] '>At CodeVanta, we are dedicated to providing you with exceptional services
-              that cater to your unique needs</p>
+    <div className="">
+      <div className="px-[7rem] sm:px-3">
+        <div className="text-center flex flex-col gap-2 ">
+          <p className="text-[4rem] text-[#225AA5] font-normal ">SERVICES</p>
+          <p className="text-[1.8rem] ">
+            At CodeVanta, we are dedicated to providing you with exceptional
+            services that cater to your unique needs
+          </p>
         </div>
-      <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#225AA5]">
-      <div className="flex pl-[3.5rem] pt-[2rem] gap-5">
-          <span className="font-bold">
-            <p className='text-[1.3rem]'>01</p>
-          </span>
-          <span className="flex flex-col gap-3">
-            <p className='w-[5rem] font-bold text-[1.3rem]'>Technology Solutions</p>
-            <p className='w-[25rem] text-[1rem]'> Lorem ipsum dolor sit amet consectetur. Porttitor morbi at massa sed feugiat ultrices. Volutpat velit donec molestie sed vitae sapien.</p>
-          </span>
-      </div>
-      </div>
-        <div>
-          <div className="service h-[100vh]">
-          <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#225AA5] flex justify-start items-start">
-          <div className="flex pl-[3.5rem] pt-[2rem] gap-5 ">
-          <span className="font-bold">
-            <p className='text-[1.3rem]'>01</p>
-          </span>
-          <span className="flex flex-col gap-3">
-            <p className='w-[5rem] font-bold text-[1.3rem] '>Technology Solutions</p>
-            <p className='w-[25rem] text-[1rem]'> Lorem ipsum dolor sit amet consectetur. Porttitor morbi at massa sed feugiat ultrices. Volutpat velit donec molestie sed vitae sapien.</p>
-          </span>
-      </div>
-      </div>
+        <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#029CD6] mt-[2.5rem]">
+          <div className="flex pl-[3.5rem] pt-[2rem] gap-5 sm:pt-[1.1rem] sm:pl-[2.2rem] sm:gap-4">
+            <span className="font-bold">
+              <p className="text-[1.3rem] sm:text-[1.25rem]">01</p>
+            </span>
+            <span className="flex flex-col gap-3 sm:gap-1">
+              <p className="w-[5rem] font-bold text-[1.3rem] sm:w-[1.8rem]  sm:text-[1.2rem]">
+                Technology Solutions
+              </p>
+              <p className="w-[25rem] text-[1rem]  sm:w-[15rem] ">
+                Lorem ipsum dolor sit amet consectetur. Porttitor morbi at massa
+                sed feugiat ultrices. Volutpat velit donec molestie sed vitae
+                sapien.
+              </p>
+            </span>
           </div>
         </div>
-
-    
-      
-    </div>
       </div>
-  )
-}
+      {/* flex items-center  justify-center flex-row flex-wrap gap-x-96 gap-y-80 */}
+      {/* <div className="service py-[6rem] px-[7rem] sm:px-3 ">
+        <div className="">
+          <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#029CD6]  ml-[40rem] sm:ml-0  ">
+            <div className="flex pl-[2.3rem] pt-[2rem] gap-5  ">
+              <span className="font-bold">
+                <p className="text-[1.3rem]">02</p>
+              </span>
+              <span className="flex flex-col gap-3">
+                <p className="w-[5rem] font-bold text-[1.3rem] sm:w-[2rem] ">
+                  Digital Transformation
+                </p>
+                <p className="w-[25rem] text-[1rem] sm:w-[15rem]">
+                  Lorem ipsum dolor sit amet consectetur. Porttitor morbi at
+                  massa sed feugiat ultrices. Volutpat velit donec molestie sed
+                  vitae sapien.
+                </p>
+              </span>
+            </div>
+          </div>
+
+          <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#029CD6] ">
+            <div className="flex pl-[3.5rem] pt-[2rem] gap-5">
+              <span className="font-bold">
+                <p className="text-[1.3rem]">03</p>
+              </span>
+              <span className="flex flex-col gap-3">
+                <p className="w-[5rem] font-bold text-[1.3rem] ">
+                  Ecommerce& Marketplace
+                </p>
+                <p className="w-[25rem] text-[1rem]  sm:w-[15rem]">
+                  Lorem ipsum dolor sit amet consectetur. Porttitor morbi at
+                  massa sed feugiat ultrices. Volutpat velit donec molestie sed
+                  vitae sapien.
+                </p>
+              </span>
+            </div>
+          </div>
+
+          <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#029CD6] ml-[39rem] sm:ml-0">
+            <div className="flex pl-[3.5rem] pt-[2rem] gap-5 ">
+              <span className="font-bold">
+                <p className="text-[1.3rem]">04</p>
+              </span>
+              <span className="flex flex-col gap-3">
+                <p className="w-[5rem] font-bold text-[1.3rem] sm:w-[2rem]">
+                  Data Securing
+                </p>
+                <p className="w-[25rem] text-[1rem]  sm:w-[15rem]">
+                  Lorem ipsum dolor sit amet consectetur. Porttitor morbi at
+                  massa sed feugiat ultrices. Volutpat velit donec molestie sed
+                  vitae sapien.
+                </p>
+              </span>
+            </div>
+          </div>
+
+          <div className="w-[12.5rem] h-[12.5rem] rounded-full bg-[#029CD6]">
+            <div className="flex pl-[3.5rem] pt-[2rem] gap-5 ">
+              <span className="font-bold">
+                <p className="text-[1.3rem]">05</p>
+              </span>
+              <span className="flex flex-col gap-3">
+                <p className="w-[11rem] font-bold text-[1.3rem] sm:w-[3rem] ">
+                  Consulting & Advisory Services
+                </p>
+                <p className="w-[25rem] text-[1rem]  sm:w-[15rem]">
+                  Lorem ipsum dolor sit amet consectetur. Porttitor morbi at
+                  massa sed feugiat ultrices. Volutpat velit donec molestie sed
+                  vitae sapien.
+                </p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
